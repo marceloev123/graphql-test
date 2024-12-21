85009650e0c9092214e95c36b4599781792a1895
marceloev123 <marceloevv08@gmail.com>
2024-12-20 19:21:47 -0500

    fix

--- codegen.ts
+++ codegen.ts
@@ -1,12 +1,16 @@
 import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
-  schema: "https://rickandmortyapi.com/graphql",
+  schema: process.env.VITE_API_URI as string,
   documents: "src/graphql/queries/**/*.graphql",
   overwrite: true,
   generates: {
     "src/graphql/schema/graphql.ts": {
-      plugins: ['typescript', 'typescript-operations', 'typescript-react-apollo'],
+      plugins: [
+        "typescript",
+        "typescript-operations",
+        "typescript-react-apollo",
+      ],
     },
     "./graphql.schema.json": {
       plugins: ["introspection"],
